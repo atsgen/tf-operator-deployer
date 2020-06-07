e2a6308fd804e4e864bd710c1828c5485282d525
Prabhjot Singh Sethi <prabhjot@atsgen.com>
2020-06-08 00:52:13 +0530

    fix download of images

--- get_operator_image.sh
+++ get_operator_image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -o tf-operator.tar http://134.119.178.86:10090/containers/tf-operator.tar
+curl -o tf-operator.tar http://134.119.178.86:12340/tf-operator.tar
 docker load -i tf-operator.tar
