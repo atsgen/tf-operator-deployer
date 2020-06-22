bc24f5d7414ffc9e6f3536509d918fbb9037a1f9
Prabhjot Singh Sethi <prabhjot@atsgen.com>
2020-06-22 11:14:48 +0530

    correct typo

--- helper/install_ubuntu.sh
+++ helper/install_ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-Sudo apt-get update -y
+sudo apt-get update -y
 sudo apt-get install -y linux-headers-$(uname -r) docker.io
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo touch /etc/apt/sources.list.d/kubernetes.list 
